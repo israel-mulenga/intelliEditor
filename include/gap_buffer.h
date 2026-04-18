c8747b19dd48ef3d91f23d9dc6f4f39d36fb6b80
mulenga-nsembe-israel <mulengaisrael5@gmail.com>
2026-04-18 12:14:16 +0200

    gap buffer init structure

---
+++ include/gap_buffer.h
@@ -0,0 +1,22 @@
+#ifndef GAP_BUFFER_H
+#define GAP_BUFFER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char *buffer;       
+    size_t gap_start;   
+    size_t gap_end;     
+    size_t buffer_size; 
+} GapBuffer;
+
+GapBuffer* gap_buffer_create(size_t initial_capacity);
+void gap_buffer_insert(GapBuffer *gb, char c);
+void gap_buffer_delete(GapBuffer *gb);
+void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position);
+void gap_buffer_destroy(GapBuffer *gb);
+char* gap_buffer_get_content(GapBuffer *gb);
+
+#endif 
