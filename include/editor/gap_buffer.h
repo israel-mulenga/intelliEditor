f40c332450373b74a5f7d16b3a5a25b4ef58573c
mulenga-nsembe-israel <mulengaisrael5@gmail.com>
2026-04-20 17:24:40 +0200

    implementing resize and destroy

--- include/editor/gap_buffer.h
+++ include/editor/gap_buffer.h
@@ -13,6 +13,7 @@ typedef struct {
 } GapBuffer;
 
 GapBuffer* gap_buffer_create(size_t initial_capacity);
+GapBuffer* gap_buffer_resize(GapBuffer *gb);
 void gap_buffer_insert(GapBuffer *gb, char c);
 void gap_buffer_delete(GapBuffer *gb);
 void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position);
--- src/editor/gap_buffer.c
+++ src/editor/gap_buffer.c
@@ -12,10 +12,12 @@ GapBuffer* gap_buffer_create(size_t initial_capacity) {
 
     gb->size = initial_capacity;
     gb->gap_start = 0;
-    gb->gap_end = 1024;
+    gb->gap_end = initial_capacity;
 
     return gb;
 }
+
+
 
 void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position) {
     size_t current_text_length = gb->size - (gb->gap_end - gb->gap_start);
@@ -25,10 +27,10 @@ void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position) {
         return;
     }
 
-    size_t distance = abs(gb->gap_start - new_position);
 
     if (new_position < gb->gap_start) {
         // MOUVEMENT VERS LA GAUCHE
+        size_t distance = gb->gap_start - new_position;
         memmove(&gb->buffer[gb->gap_end - distance], &gb->buffer[new_position], distance);
         
         gb->gap_start -= distance;
@@ -36,6 +38,7 @@ void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position) {
     } 
     else if (new_position > gb->gap_start) {
         // MOUVEMENT VERS LA DROITE
+        size_t distance = new_position - gb->gap_start;
         memmove(&gb->buffer[gb->gap_start], &gb->buffer[gb->gap_end], distance);
         
         gb->gap_start += distance;
@@ -43,11 +46,43 @@ void gap_buffer_move_cursor(GapBuffer *gb, size_t new_position) {
     }
 }
 
+void gap_buffer_destroy(GapBuffer *gb){
+    free(gb->buffer);
+    free(gb);
+}
+
+GapBuffer* gap_buffer_resize(GapBuffer *gb) {
+    size_t old_size = gb->size;
+    size_t new_size = old_size * 2;
+    
+    char *new_buffer = malloc(new_size);
+    if (!new_buffer) return NULL; 
+
+    size_t prefix_len = gb->gap_start; 
+    size_t suffix_len = old_size - gb->gap_end;
+    memcpy(new_buffer, gb->buffer, prefix_len);
+
+
+    size_t new_gap_end = new_size - suffix_len;
+    memcpy(new_buffer + new_gap_end, gb->buffer + gb->gap_end, suffix_len);
+
+    free(gb->buffer);
+    gb->buffer = new_buffer;
+    gb->size = new_size;
+    gb->gap_end = new_gap_end;
+
+    return gb;
+}
+
+
 void gap_buffer_insert(GapBuffer *gb, char c){
     if(gb->gap_start == gb->gap_end){
-        // TODO Implement RESIZE
-        
-    }
+        GapBuffer *resized_gb = gap_buffer_resize(gb);
+        if(!resized_gb){
+            fprintf(stderr, "Out of memory: insertion failed\n");
+            return;
+        }
+                }
     gb->buffer[gb->gap_start] = c;
     gb->gap_start++;
 }
