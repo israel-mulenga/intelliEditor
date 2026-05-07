b352cef572eee748d66f8df2a5ab84459ae87cbf
JoyceMUYUMBA <23bm023si@esisalama.org>
2026-05-07 08:42:57 +0000

    add a icon menu

--- src/ui/toolbar.c
+++ src/ui/toolbar.c
@@ -5,7 +5,7 @@
 
 // ======================================
 // MODULE: TOOLBAR
-// ROLE: Create action buttons
+// ROLE: Create action buttons with icons
 // ======================================
 
 GtkWidget* create_toolbar(AppWidgets *app_widgets) {
@@ -14,14 +14,38 @@ GtkWidget* create_toolbar(AppWidgets *app_widgets) {
 
     GtkWidget *file_item = gtk_menu_item_new_with_label("File");
     GtkWidget *file_menu = gtk_menu_new();
-    GtkWidget *import_item = gtk_menu_item_new_with_label("Import...");
-    GtkWidget *save_item = gtk_menu_item_new_with_label("Save");
-    GtkWidget *save_as_item = gtk_menu_item_new_with_label("Save As...");
 
+    // Créer l'élément "Import" avec icône
+    GtkWidget *import_item = gtk_menu_item_new();
+    GtkWidget *import_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);  // Boîte horizontale avec espacement
+    GtkWidget *import_image = gtk_image_new_from_icon_name("document-open", GTK_ICON_SIZE_MENU);  // Icône
+    GtkWidget *import_label = gtk_label_new("Import...");
+    gtk_box_pack_start(GTK_BOX(import_box), import_image, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(import_box), import_label, FALSE, FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(import_item), import_box);
     g_signal_connect(import_item, "activate", G_CALLBACK(on_file_import_clicked), app_widgets);
+
+    // Créer l'élément "Save" avec icône
+    GtkWidget *save_item = gtk_menu_item_new();
+    GtkWidget *save_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    GtkWidget *save_image = gtk_image_new_from_icon_name("document-save", GTK_ICON_SIZE_MENU);
+    GtkWidget *save_label = gtk_label_new("Save");
+    gtk_box_pack_start(GTK_BOX(save_box), save_image, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(save_box), save_label, FALSE, FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(save_item), save_box);
     g_signal_connect(save_item, "activate", G_CALLBACK(on_file_save_clicked), app_widgets);
+
+    // Créer l'élément "Save As" avec icône
+    GtkWidget *save_as_item = gtk_menu_item_new();
+    GtkWidget *save_as_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6);
+    GtkWidget *save_as_image = gtk_image_new_from_icon_name("document-save-as", GTK_ICON_SIZE_MENU);
+    GtkWidget *save_as_label = gtk_label_new("Save As...");
+    gtk_box_pack_start(GTK_BOX(save_as_box), save_as_image, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(save_as_box), save_as_label, FALSE, FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(save_as_item), save_as_box);
     g_signal_connect(save_as_item, "activate", G_CALLBACK(on_file_save_as_clicked), app_widgets);
 
+    // Ajouter les éléments au menu
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), import_item);
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), save_item);
     gtk_menu_shell_append(GTK_MENU_SHELL(file_menu), save_as_item);
